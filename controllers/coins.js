045f515b70c3d587aed503f5d284a8fcb7a2711b
Vaibhav <vermavaibhav2005@gmail.com>
2025-01-11 20:47:37 +0530

    fix for vercel timeout

--- controllers/coins.js
+++ controllers/coins.js
@@ -2,30 +2,28 @@ import mongoose from "mongoose";
 import Coin from "../models/coins.js";
 import PriceHistory from "../models/priceHistory.js";
 
-// const coinModel = mongoose.model("Coin");
-const statModel = mongoose.model("PriceHistory");
 
-export const addCoin = async(req,res)=>{
-    try{
-        
-        const coinName = req.body.coinName;
-        const symbol = req.body.coinSymbol;
 
-        const foundCoin = await Coin.findOne({symbol:symbol});
-        if(foundCoin){
-            return res.status(400).json({message:"Coin already exists"});
+export const addCoin = async (req, res) => {
+    try {
+        const { coinName, coinSymbol: symbol } = req.body;
+
+        // Check if the coin exists
+        const foundCoin = await Coin.findOne({ symbol });
+        if (foundCoin) {
+            return res.status(400).json({ message: "Coin already exists" });
         }
 
+        // Create and save the coin
         const newCoin = new Coin({
-            coinName:coinName,
-            symbol:symbol,
-        standardDeviation:0.0,
-            standardDeviationCreatedAt:Date.now()});
-        const createdCoin  = await newCoin.save();
-            console.log(createdCoin);
+            coinName,
+            symbol,
+            standardDeviation: 0.0,
+            standardDeviationCreatedAt: Date.now(),
+        });
 
-            const url = `https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&include_market_cap=true&include_24hr_change=true&ids=`+symbol;
-        console.log(url);
+        const url = `https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&include_market_cap=true&include_24hr_change=true&ids=`+symbol;
+        // console.log(url);
         const options = { method: 'GET', headers: { accept: 'application/json', 'x-cg-demo-api-key': process.env.COINGECKO_API_KEY } };
         let op;
         await fetch(url, options)
@@ -42,14 +40,11 @@ export const addCoin = async(req,res)=>{
             change24h:op[symbol].usd_24h_change
         });
 
-        const createdStat = await coinStat.save();
+        const [createdCoin,createdStat] = await Promise.all([newCoin.save(),coinStat.save()]);
 
-        console.log(createdStat);
-        res.status(200).json({message:"Coin added successfully", coin:createdCoin});
-
-
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message:"Internal server error"});
+        res.status(200).json({ message: "Coin added successfully", coin: createdCoin, firstStat: createdStat });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
     }
-}
+};
